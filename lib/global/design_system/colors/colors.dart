8fea62e9eeae3119bbfb7a15c7ffa8f8f4756b14
Rishabh Gupta <rishabh.gupta@yocket.in>
2024-02-21 10:53:00 +0530

    Design System Created

---
+++ lib/global/design_system/colors/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Indra {
+  const Indra._();
+
+  static const lightBlue = Color(0xff339AF0);
+
+  static const black = Colors.black;
+
+  static const blue = Colors.blue;
+
+  static const green = Colors.green;
+
+  static const orange = Colors.orange;
+
+  static const red = Colors.red;
+
+  static const white = Colors.white;
+}
---
+++ lib/global/design_system/sizing/sizing.dart
@@ -0,0 +1,45 @@
+class Sp {
+  const Sp._();
+
+  static const px4 = 4.0;
+
+  static const px8 = 8.0;
+
+  static const px10 = 10.0;
+
+  static const px12 = 12.0;
+
+  static const px20 = 20.0;
+
+  static const px30 = 30.0;
+
+  static const px40 = 40.0;
+
+  static const px80 = 80.0;
+
+  static const px100 = 100.0;
+
+  static const px120 = 120.0;
+}
+
+class Rounding {
+  const Rounding._();
+
+  static const px4 = 4.0;
+
+  static const px8 = 8.0;
+
+  static const px10 = 10.0;
+
+  static const px20 = 20.0;
+
+  static const px30 = 30.0;
+
+  static const px40 = 40.0;
+
+  static const px80 = 80.0;
+
+  static const px100 = 100.0;
+
+  static const px120 = 120.0;
+}
